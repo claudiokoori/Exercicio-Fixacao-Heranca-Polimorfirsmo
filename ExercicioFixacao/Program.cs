ccf8572d7c16de5dde267a6a3bd84e279a7f60b5
Claudio Gomes <claudiojrkoori@gmail.com>
2023-07-30 19:27:47 -0300

    Add project files.

---
+++ ExercicioFixacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioFixacao.Entities;
+
+namespace ExercicioFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numero; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                if( type == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine());
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                } else if ( type == 'u')
+                {
+                    Console.Write("Manufacture date (MM/DD/YYYY): ");
+                    DateTime dateTime = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, dateTime));
+                } else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
